f57a1e47106dd8725f01fce32fcf7102e461f2a8
vladkvashevich <vlad_r86@mail.ru>
2024-05-23 21:49:33 +0500

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -2,8 +2,8 @@ example="Тапинамбур"
 print(example[0])
 print(example[-1])
 print(example[len(example)//2:])
-print(example[len(example)::-1])
-print(example[1]+example[3]+example[5]+example[7]+example[9])
+print(example[::-1])
+print(example[1::2])
 
 
 
