122fcfa6ff3ee7a94c16940cfaf019fa207de6b5
vladkvashevich <vlad_r86@mail.ru>
2024-05-25 01:19:35 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var= 1, 2.5, "home", True
+print(immutable_var)
+#immutable_var[0]=30
+#print(immutable_var)
+mutable_list=[5, 7.5, "Здравствуйте", False]
+mutable_list[2]="До свидания"
+print(mutable_list)
