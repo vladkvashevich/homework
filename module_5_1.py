05693d7612b99d7c8ec382cf1f2da166206758c8
vladkvashevich <vlad_r86@mail.ru>
2024-06-25 20:58:50 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -4,13 +4,16 @@ class House:
         self.number_of_floors = number_of_floors
     def go_to (self, new_floor):
         a = 1
-        while a <= new_floor:
-            if new_floor > self.number_of_floors or new_floor < 1:
-                print (f"В доме '{self.name}' такого этажа не существует")
-                return a
-            else:
-                print (f"Дом '{self.name}', этаж номер {a}")
-                a += 1
+        if new_floor >= 1:
+            while a <= new_floor:
+                if new_floor > self.number_of_floors or new_floor < 1:
+                    print (f"В доме '{self.name}' такого этажа не существует")
+                    return
+                else:
+                    print (f"Дом '{self.name}', этаж номер {a}")
+                    a += 1
+        else:
+            print (f"В доме '{self.name}' такого этажа не существует")
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
